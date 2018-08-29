ab6e6b86a27bb789133da99cacd5a4169a5f4ae7
uniskoal98 <36653696+uniskoal98@users.noreply.github.com>
2018-08-29 13:12:20 +0900

    Add files via upload

--- Javascript/weapon.js
+++ Javascript/weapon.js
@@ -1,24 +1,63 @@
 $(document).ready(function() {
   // weapon 영역 애니메이션 //
 
+  var THREE_ANIMATION = null;
+  var TWO_ANIMATION = null;
   function Fade() {
     if(Floor == 1) {
       $(".weapon_class").click(function() {
+        var Weapon_ID = $(this).attr("id");
+        $(".weapon_frame_one").css("z-index","-1000");
         // 콘텐츠 선택 영역 숨기기 애니메이션 //
         Weapon_frame_one_Ani();
 
         setTimeout(function() {
           //누른 영역의 클래스를 불러오기 위함 //
-          var Weapon_ID = $(".weapon_class").attr("id");
+
 
           //각각 다른 영역을 보여주기위해 switch 문 사용 //
           switch(Weapon_ID) {
             case "warrior" :
+              TWO_ANIMATION = "warrior";
               Weapon_frame_two_Ani();
-              break;
+              $(".warrior_weapon").click(function() {
+                var Weapon_select_ID = $(this).attr("id");
+
+                switch(Weapon_select_ID) {
+                  case "Sword" :
+                    THREE_ANIMATION = "Sword";
+                    Weapon_frame_two_Ani_Close();
+                    Weapon_frame_three_Ani();
+
+                  break;
+
+                  case "Spear" :
+                    THREE_ANIMATION = "Spear";
+                    Weapon_frame_two_Ani_Close();
+                  break;
+
+                  case "Yoyo" :
+                    THREE_ANIMATION = "Yoyo";
+                    Weapon_frame_two_Ani_Close();
+                  break;
+
+                  case "Boomerangs" :
+                    THREE_ANIMATION = "Boomerangs";
+                    Weapon_frame_two_Ani_Close();
+                  break;
+
+                  case "Flails" :
+                    THREE_ANIMATION = "Flails";
+                    Weapon_frame_two_Ani_Close();
+                  break;
+
+                };
+              });
+            break;
 
             case "Ranger" :
-
+              TWO_ANIMATION = "ranger";
+              Weapon_frame_two_Ani();
             break;
 
             case "Summoner" :
@@ -26,11 +65,13 @@ $(document).ready(function() {
             break;
 
             case "Thrower" :
-
+              TWO_ANIMATION = "thrower";
+              Weapon_frame_two_Ani();
             break;
 
             case "Socerer" :
-
+              TWO_ANIMATION = "socerer";
+              Weapon_frame_two_Ani();
             break;
           };
         },700);
@@ -38,6 +79,7 @@ $(document).ready(function() {
       });
     }
     else {
+      Weapon_frame_three_Ani_Close();
       Weapon_frame_two_Ani_Close();
       Weapon_frame_one_Ani_Close();
     }
@@ -46,7 +88,6 @@ $(document).ready(function() {
 
   //선택할 무기의 종류가 나오는 애니메이션입니다.
   function Weapon_frame_one_Ani() {
-    $("#weapon_frame_one").css("z-index","4");
     $(".weapon_Content").stop().animate({
       top : "70px"
     },{duration : 700, queue : false});
@@ -59,7 +100,6 @@ $(document).ready(function() {
   }
 
   function Weapon_frame_one_Ani_Close() {
-    $("#weapon_frame_one").css("z-index","5");
     $(".weapon_Content").stop().animate({
       top : "0px"
     },{duration : 200,queue : false});
@@ -72,29 +112,149 @@ $(document).ready(function() {
   }
 
   function Weapon_frame_two_Ani() {
-    $(".weapon_frame_two_warrior").css("z-index","5");
-    $("#Sword").stop().animate({left : "0px"},200,function() {
-      $("#Spear").stop().animate({left : "0px"},200,function() {
-        $("#Yoyo").stop().animate({left : "0px"},200,function() {
-          $("#Boomerangs").stop().animate({left : "0px"},200,function() {
-            $("#Flails").stop().animate({left : "0px"},200);
+    switch(TWO_ANIMATION) {
+      case "warrior" :
+        $("#warrior_select").css("z-index","1000");
+
+        $(".weapon_frame_two_warrior").css("z-index","5");
+        $("#Sword").stop().animate({left : "0px"},200,function() {
+          $("#Spear").stop().animate({left : "0px"},200,function() {
+            $("#Yoyo").stop().animate({left : "0px"},200,function() {
+              $("#Boomerangs").stop().animate({left : "0px"},200,function() {
+                $("#Flails").stop().animate({left : "0px"},200);
+              });
+            });
+          });
+        });
+      break;
+
+      case "ranger" :
+        $("#ranger_select").css("z-index","1000");
+
+        $(".weapon_frame_two_ranger").css("z-index","5");
+        $("#Guns").stop().animate({left : "0px"},200,function() {
+          $("#Bows").stop().animate({left : "0px"},200,function() {
+            $("#Repeaters").stop().animate({left : "0px"},200,function() {
+              $("#Flamethrowers").stop().animate({left : "0px"},200,function() {
+                $("#Launchers").stop().animate({left : "0px"},200,function() {
+                    $("#Others").stop().animate({left : "0px"},200);
+                });
+              });
+            });
+          });
+        });
+      break;
+      case "thrower" :
+      $("#thrower_select").css("z-index","1000");
+
+      $(".weapon_frame_two_thrower").css("z-index","5");
+      $("#Consumeables").stop().animate({left : "0px"},200,function() {
+        $("#Throw_Boomerangs").stop().animate({left : "0px"},200,function() {
+          $("#Throw_Others").stop().animate({left : "0px"},200,);
+        });
+      });
+      break;
+
+      case "socerer" :
+      $("#socerer_select").css("z-index","1000");
+
+      $(".weapon_frame_two_socerer").css("z-index","5");
+      $("#Wand").stop().animate({left : "0px"},200,function() {
+        $("#SpellTome").stop().animate({left : "0px"},200,function() {
+          $("#MagicGun").stop().animate({left : "0px"},200,function() {
+            $("#Socerer_Others").stop().animate({left : "0px"},200);
           });
         });
       });
-    });
+      break;
+
+    };
+
   }
 
   function Weapon_frame_two_Ani_Close() {
-    $(".weapon_frame_two_warrior").css("z-index","4");
-    $("#Sword").stop().animate({left : "-100%"},50,function() {
-      $("#Spear").stop().animate({left : "-100%"},50,function() {
-        $("#Yoyo").stop().animate({left : "-100%"},50,function() {
-          $("#Boomerangs").stop().animate({left : "-100%"},50,function() {
-            $("#Flails").stop().animate({left : "-100%"},50);
+    switch(TWO_ANIMATION) {
+      case "warrior" :
+      $(".weapon_frame_one").css("z-index","10");
+      $("#warrior_select").css("z-index","0");
+
+      $(".weapon_frame_two_warrior").css("z-index","4");
+      $("#Flails").stop().animate({left : "-100%"},50,function() {
+        $("#Boomerangs").stop().animate({left : "-100%"},50,function() {
+          $("#Yoyo").stop().animate({left : "-100%"},50,function() {
+            $("#Spear").stop().animate({left : "-100%"},50,function() {
+              $("#Sword").stop().animate({left : "-100%"},50);
+            });
           });
         });
       });
-    });
+      break;
+
+      case "ranger" :
+      $(".weapon_frame_one").css("z-index","10");
+      $("#ranger_select").css("z-index","0");
+
+      $(".weapon_frame_two_ranger").css("z-index","5");
+      $("#Guns").stop().animate({left : "-100%"},50,function() {
+        $("#Bows").stop().animate({left : "-100%"},50,function() {
+          $("#Repeaters").stop().animate({left : "-100%"},50,function() {
+            $("#Flamethrowers").stop().animate({left : "-100%"},50,function() {
+              $("#Launchers").stop().animate({left : "-100%"},50,function() {
+                  $("#Others").stop().animate({left : "-100%"},50);
+              });
+            });
+          });
+        });
+      });
+      break;
+
+      case "thrower" :
+      $(".weapon_frame_one").css("z-index","10");
+      $("#thrower_select").css("z-index","0");
+
+      $(".weapon_frame_two_thrower").css("z-index","5");
+      $("#Consumeables").stop().animate({left : "-100%"},50,function() {
+        $("#Throw_Boomerangs").stop().animate({left : "-100%"},50,function() {
+          $("#Throw_Others").stop().animate({left : "-100%"},50,);
+        });
+      });
+      break;
+      case "socerer" :
+      $(".weapon_frame_one").css("z-index","10");
+      $("#socerer_select").css("z-index","0");
+
+      $(".weapon_frame_two_socerer").css("z-index","5");
+      $("#Wand").stop().animate({left : "-100%"},50,function() {
+        $("#SpellTome").stop().animate({left : "-100%"},50,function() {
+          $("#MagicGun").stop().animate({left : "-100%"},50,function() {
+            $("#Socerer_Others").stop().animate({left : "-100%"},50);
+          });
+        });
+      });
+      break;
+    }
+
+  }
+
+  function Weapon_frame_three_Ani() {
+    if(THREE_ANIMATION == "Sword") {
+      $(".weapon_frame_one").css("z-index","-1000");
+      $("#weapon_frame_three_warrior").css("z-index","1000");
+
+      setTimeout(function() {
+        $("#weapon_frame_three_warrior").fadeIn(500);
+      }, 200);
+    }
+  }
+
+  function Weapon_frame_three_Ani_Close() {
+    if(THREE_ANIMATION == "Sword") {
+      $(".weapon_frame_one").css("z-index","10");
+      $("#weapon_frame_three_warrior").css("z-index","0");
+
+      $("#weapon_frame_three_warrior").fadeOut(50);
+    }
+
   }
 
 
